f51d204d5425a730f49fa2cfd3fdd3254d71787f
OneTrick475 <ibukev4@gmail.com>
2024-05-21 22:30:52 +0300

    dgh

--- src/Logger.java
+++ src/Logger.java
@@ -1,4 +1,4 @@
 public interface Logger {
     void logFailedLogin(String username, String ip);
-    void logAdminOperation(String );
+    void logAdminOperation(String type, String username, String ip);
 }
